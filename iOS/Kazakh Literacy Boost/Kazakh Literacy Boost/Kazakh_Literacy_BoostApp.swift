4ca2694f6f8df1f2dcc280049703afce89c9e0f8
Yedige Ashirbek <yedigeashirbek@ZhaiBook.local>
2024-04-13 13:23:21 +0500

    first commit

---
+++ iOS/Kazakh Literacy Boost/Kazakh Literacy Boost/Kazakh_Literacy_BoostApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kazakh_Literacy_BoostApp.swift
+//  Kazakh Literacy Boost
+//
+//  Created by Yedige Ashirbek on 13.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct Kazakh_Literacy_BoostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
